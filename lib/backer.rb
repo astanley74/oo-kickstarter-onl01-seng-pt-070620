b5a7dbf7c8a1196f56d31af6c20a4cf3ffe9e56c
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-30 19:19:03 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,13 @@
 class Backer
   attr_reader :backed_projects, :name
+  
   def initialize(name)
     @name = name
     @backed_projects = []
+  end
+  
+  def back_project(project)
+    project = Project.new
+    @backed_projects << project
   end
 end
